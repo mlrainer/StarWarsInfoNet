b69c173ecf0beb8433b45d20077c92b0947fb96f
mlrainer <112909069+mlrainer@users.noreply.github.com>
2023-11-05 13:09:29 -0500

    Got json data from api to display on first page of people

--- scripts/Pagesjs/people.js
+++ scripts/Pagesjs/people.js
@@ -1,4 +1,4 @@
-// Variables People
+const ctnPeople = document.getElementById("ctn-main");
 const peoplePrevious = document.getElementById("people-previous");
 const peopleNext = document.getElementById("people-next");
 
@@ -9,29 +9,29 @@ let previousPeople;
 peoplePrevious.addEventListener("click", pagePreviousPeople);
 peopleNext.addEventListener("click", pageNextPeople);
 
-// Functions People
+
 async function fetchPeople() {
   document.querySelector('.overlay').classList.add('active');
-    let results = await fetch(URL_People);
-    const data = await results.json();
-    nextPeople = data.next;
-    previousPeople = data.previous;
-    let people = data.results;
-    let outPut = ' ';
-  document.querySelector('.overlay').classList.remove('active');  
-    people.forEach(item => {
-      outPut += `<div class="card card-people">
-                    <h2>${item.name}</h2>
-                    <h5>Gender: ${item.gender}</h5>
-                    <h5>Birth Year: ${item.birth_year}</h5>
-                    <h5>Height: ${item.height}</h5>
-                    <h5>Hair Color: ${item.hair_color}</h5>
-                    <h5>Skin Color: ${item.skin_color}</h5>
-                    <h5>Eye Color: ${item.eye_color}</h5>
-                 </div>`
-    })
-    ctnPlanets.innerHTML = outPut;
-  }
+  let results = await fetch(URL_People);
+  const data = await results.json();
+  nextPeople = data.next;
+  previousPeople = data.previous;
+  let people = data.results;
+  let outPut = ' ';
+  document.querySelector('.overlay').classList.remove('active');
+  people.forEach(item => {
+    outPut += `<div class="card card-people">
+                  <h2>${item.name}</h2>
+                  <h5>Gender: ${item.gender}</h5>
+                  <h5>Birth Year: ${item.birth_year}</h5>
+                  <h5>Height: ${item.height}</h5>
+                  <h5>Hair Color: ${item.hair_color}</h5>
+                  <h5>Skin Color: ${item.skin_color}</h5>
+                  <h5>Eye Color: ${item.eye_color}</h5>
+               </div>`
+  })
+  ctnPeople.innerHTML = outPut;
+}
   
   function pageNextPeople() {
     if(nextPeople) {
